90f43380b8d36622f42806f369efb6b54e2e0a6d
cypl <cyrille.plenchette@me.com>
2023-04-19 11:06:17 +0200

    add color starter and lightness areas

--- src/App.js
+++ src/App.js
@@ -2,51 +2,53 @@ import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import ColorPanel from './components/ColorPanel'
 import { lightScale } from './utils'
+import { colorStarter } from './utils/colorStarter'
+import { lightnessArea } from './utils/lightnessArea'
 
 function App() {
-  const [primary1, setPrimary1] = useState('#4C73DD')
+  const [primary1, setPrimary1] = useState(colorStarter.primary1)
   const [primary1Scale, setPrimary1Scale] = useState(lightScale(primary1))
   useEffect(() => {
     setPrimary1Scale(lightScale(primary1))
   }, [primary1, primary1Scale])
 
-  const [primary, setPrimary] = useState('#1E3288')
+  const [primary, setPrimary] = useState(colorStarter.primary)
   const [primaryScale, setPrimaryScale] = useState(lightScale(primary))
   useEffect(() => {
     setPrimaryScale(lightScale(primary))
   }, [primary, primaryScale])
 
-  const [primary2, setPrimary2] = useState('#0D0A33')
+  const [primary2, setPrimary2] = useState(colorStarter.primary2)
   const [primary2Scale, setPrimary2Scale] = useState(lightScale(primary2))
   useEffect(() => {
     setPrimary2Scale(lightScale(primary2))
   }, [primary2, primary2Scale])
 
-  const [secondary1, setSecondary1] = useState('#3fde83')
+  const [secondary1, setSecondary1] = useState(colorStarter.secondary1)
   const [secondary1Scale, setSecondary1Scale] = useState(lightScale(secondary1))
   useEffect(() => {
     setSecondary1Scale(lightScale(secondary1))
   }, [secondary1, secondary1Scale])
 
-  const [secondary, setSecondary] = useState('#047C4E')
+  const [secondary, setSecondary] = useState(colorStarter.secondary)
   const [secondaryScale, setSecondaryScale] = useState(lightScale(secondary))
   useEffect(() => {
     setSecondaryScale(lightScale(secondary))
   }, [secondary, secondaryScale])
 
-  const [secondary2, setSecondary2] = useState('#153224')
+  const [secondary2, setSecondary2] = useState(colorStarter.secondary2)
   const [secondary2Scale, setSecondary2Scale] = useState(lightScale(secondary2))
   useEffect(() => {
     setSecondary2Scale(lightScale(secondary2))
   }, [secondary2, secondary2Scale])
 
-  const [clair, setClair] = useState('#FAF5F5')
+  const [clair, setClair] = useState(colorStarter.clair)
   const [clairScale, setClairScale] = useState(lightScale(clair))
   useEffect(() => {
     setClairScale(lightScale(clair))
   }, [clair, clairScale])
 
-  const [clair2, setClair2] = useState('#D1CECE')
+  const [clair2, setClair2] = useState(colorStarter.clair2)
   const [clair2Scale, setClair2Scale] = useState(lightScale(clair2))
   useEffect(() => {
     setClair2Scale(lightScale(clair2))
@@ -78,24 +80,24 @@ function App() {
               colorScale={primary1Scale}
               onClickFunc={(event) => openPicker(event)}
               onChangeFunc={(event) => changeColor(event, setPrimary1)}
-              lowLimit={10}
-              highLimit={50}
+              lowLimit={lightnessArea.find((c) => c.color === 'primary1').low}
+              highLimit={lightnessArea.find((c) => c.color === 'primary1').up}
             />
             <ColorPanel
               colorHex={primary}
               colorScale={primaryScale}
               onClickFunc={(event) => openPicker(event)}
               onChangeFunc={(event) => changeColor(event, setPrimary)}
-              lowLimit={10}
-              highLimit={50}
+              lowLimit={lightnessArea.find((c) => c.color === 'primary').low}
+              highLimit={lightnessArea.find((c) => c.color === 'primary').up}
             />
             <ColorPanel
               colorHex={primary2}
               colorScale={primary2Scale}
               onClickFunc={(event) => openPicker(event)}
               onChangeFunc={(event) => changeColor(event, setPrimary2)}
-              lowLimit={10}
-              highLimit={50}
+              lowLimit={lightnessArea.find((c) => c.color === 'primary2').low}
+              highLimit={lightnessArea.find((c) => c.color === 'primary2').up}
             />
           </div>
         </div>
@@ -107,24 +109,24 @@ function App() {
               colorScale={secondary1Scale}
               onClickFunc={(event) => openPicker(event)}
               onChangeFunc={(event) => changeColor(event, setSecondary1)}
-              lowLimit={10}
-              highLimit={50}
+              lowLimit={lightnessArea.find((c) => c.color === 'secondary1').low}
+              highLimit={lightnessArea.find((c) => c.color === 'secondary1').up}
             />
             <ColorPanel
               colorHex={secondary}
               colorScale={secondaryScale}
               onClickFunc={(event) => openPicker(event)}
               onChangeFunc={(event) => changeColor(event, setSecondary)}
-              lowLimit={10}
-              highLimit={50}
+              lowLimit={lightnessArea.find((c) => c.color === 'secondary').low}
+              highLimit={lightnessArea.find((c) => c.color === 'secondary').up}
             />
             <ColorPanel
               colorHex={secondary2}
               colorScale={secondary2Scale}
               onClickFunc={(event) => openPicker(event)}
               onChangeFunc={(event) => changeColor(event, setSecondary2)}
-              lowLimit={10}
-              highLimit={50}
+              lowLimit={lightnessArea.find((c) => c.color === 'secondary2').low}
+              highLimit={lightnessArea.find((c) => c.color === 'secondary2').up}
             />
           </div>
         </div>
@@ -136,16 +138,16 @@ function App() {
               colorScale={clairScale}
               onClickFunc={(event) => openPicker(event)}
               onChangeFunc={(event) => changeColor(event, setClair)}
-              lowLimit={10}
-              highLimit={50}
+              lowLimit={lightnessArea.find((c) => c.color === 'clair').low}
+              highLimit={lightnessArea.find((c) => c.color === 'clair').up}
             />
             <ColorPanel
               colorHex={clair2}
               colorScale={clair2Scale}
               onClickFunc={(event) => openPicker(event)}
               onChangeFunc={(event) => changeColor(event, setClair2)}
-              lowLimit={10}
-              highLimit={50}
+              lowLimit={lightnessArea.find((c) => c.color === 'clair2').low}
+              highLimit={lightnessArea.find((c) => c.color === 'clair2').up}
             />
           </div>
         </div>
---
+++ src/utils/lightnessArea.js
@@ -0,0 +1,45 @@
+/**
+ * L'intensité lumineuse d'une couleur peut aller de 0 (low) à 100 (up)
+ */
+export const lightnessArea = [
+  {
+    color: 'primary1',
+    low: 53,
+    up: 58,
+  },
+  {
+    color: 'primary',
+    low: 25,
+    up: 30,
+  },
+  {
+    color: 'primary2',
+    low: 12,
+    up: 17,
+  },
+  {
+    color: 'secondary1',
+    low: 53,
+    up: 58,
+  },
+  {
+    color: 'secondary',
+    low: 25,
+    up: 30,
+  },
+  {
+    color: 'secondary2',
+    low: 12,
+    up: 17,
+  },
+  {
+    color: 'clair',
+    low: 92,
+    up: 97,
+  },
+  {
+    color: 'clair2',
+    low: 80,
+    up: 90,
+  },
+]
